3277ada5d6d137ef3af56db22f53c17cbc478f4a
Nicolas Strike <strikenp@msoe.edu>
2019-06-13 19:21:01 -0500

    Added VERY basic controls. When p is pressed (anywhere on the computer) spotify will pause, and s will play.

--- src/testing.py
+++ src/testing.py
@@ -1,6 +1,7 @@
 import sys
 import spotipy
 import spotipy.util as util
+from pynput.keyboard import Key, Controller, Listener
 
 SPOTIPY_CLIENT_ID='af93d78862ad47bcbbcc14a7f9712a60'
 SPOTIPY_CLIENT_SECRET='2e498f3cc02a4e65801c27fd20168ca3'
@@ -16,10 +17,33 @@ else:
     sys.exit()
 
 token = util.prompt_for_user_token(USERNAME, scope, client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI)
+sp = spotipy.Spotify(auth=token)
 
 if token:
-    sp = spotipy.Spotify(auth=token)
-    sp.pause_playback()
+    # sp.pause_playback()
+    keyboard = Controller()
+
+def on_press(key):
+    print('{0} pressed'.format(key))
+    if key.char == 'p':
+        print("Pausing playback")
+        sp.pause_playback()
+    if key.char == 's':
+        print("Starting playback")
+        sp.start_playback()
+
+
+def on_release(key):
+    print('{0} release'.format(
+        key))
+    if key == Key.esc:
+        # Stop listener
+        return False
+
+with Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
     # client = spotipy.client.Spotify(auth=token)
 #     results = sp.current_user_saved_tracks()
 #     for item in results['items']:
